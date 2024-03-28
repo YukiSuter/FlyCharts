19ffd531ff535449d3ea7250a139a658f5c21a69
YukiSuter <yukisuter@ycsuter.dev>
2024-03-28 02:13:11 +0000

    New Home UI

---
+++ lib/widgets/bottomBar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class bottomBar extends StatefulWidget {
+  const bottomBar({super.key});
+
+  @override
+  State<bottomBar> createState() => _bottomBarState();
+}
+
+class _bottomBarState extends State<bottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color(0xffE7E7E7),
+        height: 25,
+        width: MediaQuery.of(context).size.width,
+        child: Text(
+          "NO FLIGHT PLAN LOADED",
+          textAlign: TextAlign.center,
+        ));
+  }
+}
